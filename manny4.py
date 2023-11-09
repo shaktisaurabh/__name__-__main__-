f0c1369703bbc2f5ab79c7dc23c6ae1ddff4923e
shaktisaurabh <132367856+shaktisaurabh@users.noreply.github.com>
2023-11-09 13:46:40 +0530

    Add files via upload

---
+++ manny4.py
@@ -0,0 +1,7 @@
+import manny3
+
+a=int(input("Enter a number? "))
+print(manny3.square(a))
+
+#This demonstartes how if __name__=="__main__" statement can be used as code blocker with respect to
+#those files that import the entire codebase and as you can see that the entire manny3 file is not executed.
